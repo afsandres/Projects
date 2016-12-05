6d7a3f2748dc7ac3cdddc63b90c53d996236d138
Andres Sanchez <andres.sanchez@nebulae.com.co>
2016-12-05 17:41:47 -0500

    bala

---
+++ src/main/java/co/com/nebulae/course/world/labs/CannonBall.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.nebulae.course.world.labs;
+
+/**
+ *
+ * @author Andres Sanchez - nebulae.com.co
+ */
+public class CannonBall {
+    
+}
