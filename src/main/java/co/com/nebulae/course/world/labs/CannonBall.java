35fd2337e0b45cb115676d2dd635f60ba90e8859
Andres Sanchez <andres.sanchez@nebulae.com.co>
2016-12-07 07:35:27 -0500

    creación de bala del cañon

--- src/main/java/co/com/nebulae/course/world/labs/CannonBall.java
+++ src/main/java/co/com/nebulae/course/world/labs/CannonBall.java
@@ -5,10 +5,39 @@
  */
 package co.com.nebulae.course.world.labs;
 
+import co.com.nebulae.course.world.WorldShape;
+import javafx.event.Event;
+import javafx.scene.input.KeyEvent;
+
 /**
  *
  * @author Andres Sanchez - nebulae.com.co
  */
-public class CannonBall {
+public class CannonBall implements WorldShape {
+
+    @Override
+    public void redraw(Long time) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void handleInput(KeyEvent event) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void handleControlInput(Event event) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void isVisible() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void collision() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
