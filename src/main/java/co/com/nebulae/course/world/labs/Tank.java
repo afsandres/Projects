dedf45f665553b1ab6398ab816fe81965f695c6c
Andres Sanchez <andres.sanchez@nebulae.com.co>
2016-12-12 08:41:49 -0500

    modificacion

--- src/main/java/co/com/nebulae/course/world/labs/Tank.java
+++ src/main/java/co/com/nebulae/course/world/labs/Tank.java
@@ -24,9 +24,16 @@ public class Tank implements WorldShape {
     private Xform world;
     private Xform tankXform;
     private Xform canonXform;
-
     private Box skeleton;
     private Cylinder canonCylinder;
+    private Double angleTankX = 0d;
+    private Double angleCannonX;
+    private Double angleCannonY;
+    private Double angleCannonZ;
+    private Double speedX = 0d;
+    private Double speedZ = 0d;
+    private Double directionX = 0d;
+    private Double directionZ = 0d;
 
     final Xform elementsGroup = new Xform();
 
@@ -41,31 +48,48 @@ public class Tank implements WorldShape {
         oliveGreenMaterial.setSpecularColor(Color.OLIVE);
 
         tankXform = new Xform();
-        skeleton = new Box(150d, 50d, 300d);
-        skeleton.setTranslateX(100d);
-        skeleton.setTranslateY(19d);
-        skeleton.setTranslateZ(360d);
+        skeleton = new Box(50d, 20d, 150d);
+        skeleton.setTranslateX(0d);
+        skeleton.setTranslateY(25d);
+        skeleton.setTranslateZ(0d);
         skeleton.setMaterial(greenMaterial);
         tankXform.getChildren().add(skeleton);
 
         canonXform = new Xform();
-        canonCylinder = new Cylinder(20d, 100d);
-        canonCylinder.setTranslateX(50d);
-        canonCylinder.setTranslateY(5d);
-        canonCylinder.setTranslateZ(360d);
+        canonCylinder = new Cylinder(20d, 50d);
+        canonCylinder.setTranslateX(0d);
+        canonCylinder.setTranslateY(50d);
+        canonCylinder.setTranslateZ(0d);
         canonCylinder.setMaterial(oliveGreenMaterial);
         canonXform.getChildren().add(canonCylinder);
-
+        tankXform.getChildren().add(canonXform);
         elementsGroup.getChildren().add(tankXform);
-        elementsGroup.getChildren().add(canonXform);
         world.getChildren().addAll(elementsGroup);
 
     }
 
     public void move(Long time) {
-
+        //MRU X
+        double x = tankXform.t.getX() + directionX * speedX * time;
+        if (x < -1000) {
+            x = -1000;
+        } else if (x > 1000) {
+            x = 1000;
+        }
+        tankXform.t.setX(x);
+        tankXform.rx.setAngle(angleTankX);
+        double z = tankXform.t.getZ() + directionZ * speedZ * time;
+        if (z < -1000) {
+            z = -1000;
+        } else if (z > 1000) {
+            z = 1000;
+        }
+        System.out.println("Mover z " + x);
+        System.err.println("posición z n" + tankXform.t.getZ());
+        tankXform.t.setZ(z);
+        System.out.println("Mover z n " + x);
+        System.err.println("posición z n" + tankXform.t.getZ());
     }
-
 
     @Override
     public void redraw(Long time) {
@@ -74,21 +98,45 @@ public class Tank implements WorldShape {
 
     @Override
     public void handleInput(KeyEvent event) {
-        switch (event.getCode()) {
-            case W:
-                System.out.println("W");
-                break;
-            case A:
-                System.out.println("A");
-                break;
-            case D:
-                System.out.println("D");
-                break;
-            case S:
-                System.out.println("S");
-                break;
-            default:
-                break;
+        if (event.getEventType() == event.KEY_PRESSED) {
+            switch (event.getCode()) {
+                case W:
+                    System.out.println("W");
+                    directionZ = tankXform.getTranslateZ() + 1d;
+                    speedZ = 0.1d;
+                    break;
+                case A:
+                    System.out.println("A");
+                    angleTankX = tankXform.rx.getAngle() + 1d;
+                    directionX = tankXform.getTranslateX() + 1d;
+                    speedX = 0.1d;
+                    break;
+                case D:
+                    System.out.println("D");
+                    angleTankX = tankXform.rx.getAngle() - 1d;
+                    directionX = tankXform.getTranslateX() - 1d;
+                    speedX = 0.1d;
+                    break;
+                case S:
+                    System.out.println("S");
+                    directionZ = tankXform.getTranslateZ() - 1d;
+                    speedZ = 0.1d;
+                    break;
+                case UP:
+                    System.out.println("UP");
+                    break;
+                case DOWN:
+                    System.out.println("DOWN");
+                    break;
+                case RIGHT:
+                    System.out.println("RIGHT");
+                    break;
+                case LEFT:
+                    System.out.println("LEFT");
+                    break;
+                default:
+                    break;
+            }
         }
 
     }
@@ -104,7 +152,8 @@ public class Tank implements WorldShape {
     }
 
     @Override
-    public void handleControlInput(Event event) {
+    public void handleControlInput(Event event
+    ) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
